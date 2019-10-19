eec426a8a75ddb89cc755f40138ca1dcf505449d
WuHuan <kof1234@sina.com>
2019-10-19 09:28:21 +0800

    打开外部DLL工程和LUA工程的修改

--- PackagesCustom/com.wuhuan.consoletiny/Editor/ScriptAssetOpener.cs
+++ PackagesCustom/com.wuhuan.consoletiny/Editor/ScriptAssetOpener.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Reflection;
 using System.Threading;
+using Microsoft.Win32;
 using UnityEditor;
 using UnityEditorInternal;
 #if UNITY_2017_1_OR_NEWER
@@ -15,96 +13,9 @@ namespace ConsoleTiny
 {
     public class ScriptAssetOpener
     {
-        private Assembly m_Assembly;
-        private Type m_ScriptOpenerType;
-        private MethodInfo m_WaitForPongFromVisualStudioMi;
-        private MethodInfo m_TryOpenFileMi;
-        private MethodInfo m_VisualStudioProcessesMi;
-        private FieldInfo m_VsProcessFi;
-        private MethodInfo m_GetWindowText;
-        private MethodInfo m_VisualStudioExecutable;
-        //private MethodInfo m_AllowSetForegroundWindow;
-        private object m_ScriptOpener;
-        private string m_SolutionFile;
-
-        public bool initialized { get { return m_Assembly != null; } }
-        public bool alreadyInitialized { get; private set; }
-
-        public void Init(Assembly assembly)
+        private static bool IsNotWindowsEditor()
         {
-            alreadyInitialized = true;
-            if (assembly == null)
-            {
-                return;
-            }
-
-            m_Assembly = assembly;
-            if (m_Assembly == null)
-            {
-                return;
-            }
-            m_ScriptOpenerType = m_Assembly.GetType("SyntaxTree.VisualStudio.Unity.Bridge.ScriptOpener");
-            if (m_ScriptOpenerType == null)
-            {
-                return;
-            }
-            m_WaitForPongFromVisualStudioMi = m_ScriptOpenerType.GetMethod("WaitForPongFromVisualStudio", BindingFlags.Public | BindingFlags.Instance);
-            m_TryOpenFileMi = m_ScriptOpenerType.GetMethod("TryOpenFile", BindingFlags.Public | BindingFlags.Instance);
-            m_VisualStudioProcessesMi = m_ScriptOpenerType.GetMethod("VisualStudioProcesses", BindingFlags.NonPublic | BindingFlags.Static);
-            m_VsProcessFi = m_ScriptOpenerType.GetField("_vsProcess", BindingFlags.NonPublic | BindingFlags.Instance);
-
-            Type win32Type = m_Assembly.GetType("SyntaxTree.VisualStudio.Unity.Bridge.Win32");
-            m_GetWindowText = win32Type.GetMethod("GetWindowText", BindingFlags.Public | BindingFlags.Static, null,
-                new Type[] { typeof(int) }, null);
-            //m_AllowSetForegroundWindow = win32Type.GetMethod("AllowSetForegroundWindow", BindingFlags.Public | BindingFlags.Static);
-
-            Type productInfoType = m_Assembly.GetType("SyntaxTree.VisualStudio.Unity.ProductInfo");
-            m_VisualStudioExecutable = productInfoType.GetMethod("VisualStudioExecutable", BindingFlags.Public | BindingFlags.Static);
-        }
-
-        private bool TryAcquireVisualStudioProcess()
-        {
-            IEnumerable<Process> processes = (IEnumerable<Process>)m_VisualStudioProcessesMi.Invoke(null, null);
-            Process process = processes.FirstOrDefault(IsTargetVisualStudio);
-            m_VsProcessFi.SetValue(m_ScriptOpener, process);
-            return process != null;
-        }
-
-        private bool IsTargetVisualStudio(Process process)
-        {
-            string title = (string)m_GetWindowText.Invoke(null, new object[] { process.Id });
-            if (!string.IsNullOrEmpty(title) && title.StartsWith(Path.GetFileNameWithoutExtension(SolutionFile()), StringComparison.OrdinalIgnoreCase))
-                return true;
-            return false;
-        }
-
-        private void StartVisualStudioProcess()
-        {
-            string path = (string)m_VisualStudioExecutable.Invoke(null, null);
-            if (string.IsNullOrEmpty(path))
-            {
-                return;
-            }
-
-            string arguments = QuotePathIfNeeded(SolutionFile());
-            Process process = Process.Start(new ProcessStartInfo
-            {
-                WorkingDirectory = Path.GetFullPath(Path.GetDirectoryName(SolutionFile())),
-                UseShellExecute = true,
-                CreateNoWindow = true,
-                Arguments = arguments,
-                FileName = Path.GetFullPath(path)
-            });
-            m_VsProcessFi.SetValue(m_ScriptOpener, process);
-        }
-
-        /// <summary>
-        /// sln完整路径
-        /// </summary>
-        /// <returns></returns>
-        private string SolutionFile()
-        {
-            return m_SolutionFile;
+            return UnityEngine.Application.platform != UnityEngine.RuntimePlatform.WindowsEditor;
         }
 
         private static string QuotePathIfNeeded(string path)
@@ -116,123 +27,6 @@ namespace ConsoleTiny
             return "\"" + path + "\"";
         }
 
-        public void OpenEditor(string projectPath, string file, int line)
-        {
-            m_SolutionFile = projectPath;
-
-            if (m_ScriptOpenerType != null)
-            {
-                ThreadPool.QueueUserWorkItem(_ =>
-                {
-                    OpenEditorInter(projectPath, file, line);
-                });
-            }
-            else
-            {
-#if UNITY_2017_1_OR_NEWER
-                string vsPath = ScriptEditorUtility.GetExternalScriptEditor();
-#else
-                string vsPath = InternalEditorUtility.GetExternalScriptEditor();
-#endif
-                if (string.IsNullOrEmpty(vsPath) || !File.Exists(vsPath))
-                {
-                    return;
-                }
-                string exePath = String.Empty;
-
-#if UNITY_2018_1_OR_NEWER
-                var packageInfos = Packages.GetAll();
-                foreach (var packageInfo in packageInfos)
-                {
-                    if (packageInfo.name == "com.wuhuan.consoletiny")
-                    {
-                        exePath = packageInfo.resolvedPath;
-                        break;
-                    }
-                }
-
-#elif UNITY_2017_1_OR_NEWER
-                // TODO
-                exePath = "../../PackagesCustom/com.wuhuan.consoletiny";
-#endif
-
-                if (!string.IsNullOrEmpty(exePath))
-                {
-                    exePath = exePath + "\\Editor\\VisualStudioFileOpenTool.exe";
-
-                    ThreadPool.QueueUserWorkItem(_ =>
-                    {
-                        OpenEditorInter2(exePath, vsPath, projectPath, file, line);
-                    });
-                }
-            }
-        }
-
-        private void OpenEditorInter(string projectPath, string file, int line)
-        {
-            m_ScriptOpener = Activator.CreateInstance(m_ScriptOpenerType, projectPath, file, line - 1);
-            try
-            {
-                if (!TryAcquireVisualStudioProcess())
-                {
-                    StartVisualStudioProcess();
-                }
-                if ((bool)m_WaitForPongFromVisualStudioMi.Invoke(m_ScriptOpener, null))
-                {
-                    m_TryOpenFileMi.Invoke(m_ScriptOpener, null);
-                }
-            }
-            finally
-            {
-                if (m_ScriptOpener != null)
-                {
-                    ((IDisposable)m_ScriptOpener).Dispose();
-                }
-                m_ScriptOpener = null;
-            }
-        }
-
-        private void OpenEditorInter2(string exePath, string vsPath, string projectPath, string file, int line)
-        {
-            if (!File.Exists(exePath))
-            {
-                return;
-            }
-            
-            Process.Start(new ProcessStartInfo
-            {
-                FileName = exePath,
-                Arguments = String.Format("{0} {1} {2} {3}",
-                    QuotePathIfNeeded(vsPath), QuotePathIfNeeded(projectPath), QuotePathIfNeeded(file), line),
-                UseShellExecute = false,
-                CreateNoWindow = true
-            });
-        }
-
-        private static ScriptAssetOpener sao;
-
-        private static void LoadScriptAssetOpener()
-        {
-            if (sao == null)
-            {
-                sao = new ScriptAssetOpener();
-            }
-            if (sao.initialized || sao.alreadyInitialized)
-            {
-                return;
-            }
-
-            sao.Init(null);
-            foreach (Assembly a in AppDomain.CurrentDomain.GetAssemblies())
-            {
-                if (a.FullName.StartsWith("SyntaxTree.VisualStudio.Unity.Bridge"))
-                {
-                    sao.Init(a);
-                    break;
-                }
-            }
-        }
-
         public static bool OpenAsset(string file, int line)
         {
             if (string.IsNullOrEmpty(file) || file == "None")
@@ -241,15 +35,33 @@ namespace ConsoleTiny
             }
             if (file.StartsWith("Assets/"))
             {
+                var ext = Path.GetExtension(file).ToLower();
+                if (ext == ".lua" && TryOpenLuaFile(file, line))
+                {
+                    return true;
+                }
+
                 var obj = AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(file);
                 if (obj)
                 {
                     AssetDatabase.OpenAsset(obj, line);
+                    return true;
                 }
+
                 return false;
             }
 
-            string fileFullPath = Path.GetFullPath(file.Replace('/', '\\'));
+            char separatorChar = '\\';
+            string fileFullPath;
+            if (IsNotWindowsEditor())
+            {
+                separatorChar = '/';
+                fileFullPath = Path.GetFullPath(file);
+            }
+            else
+            {
+                fileFullPath = Path.GetFullPath(file.Replace('/', separatorChar));
+            }
 
 #if UNITY_2018_1_OR_NEWER
             var packageInfos = Packages.GetAll();
@@ -265,13 +77,39 @@ namespace ConsoleTiny
             // TODO
 #endif
 
-            LoadScriptAssetOpener();
-            if (!sao.initialized)
+            // 别人编译的DLL，不存在文件路径，那么就以工程路径拼接组装来尝试获取本地路径
+            if (!File.Exists(fileFullPath))
             {
-                return false;
+                string directoryName = Directory.GetCurrentDirectory();
+                while (true)
+                {
+                    if (string.IsNullOrEmpty(directoryName) || !Directory.Exists(directoryName))
+                    {
+                        return false;
+                    }
+
+                    int pos = fileFullPath.IndexOf(separatorChar);
+                    while (pos != -1)
+                    {
+                        string testFullPath = Path.Combine(directoryName, fileFullPath.Substring(pos + 1));
+                        if (File.Exists(testFullPath) && TryOpenVisualStudioFile(testFullPath, line))
+                        {
+                            return true;
+                        }
+
+                        pos = fileFullPath.IndexOf(separatorChar, pos + 1);
+                    }
+
+                    directoryName = Path.GetDirectoryName(directoryName);
+                }
             }
 
-            string dirPath = fileFullPath;
+            return TryOpenVisualStudioFile(fileFullPath, line);
+        }
+
+        private static bool TryOpenVisualStudioFile(string file, int line)
+        {
+            string dirPath = file;
 
             do
             {
@@ -281,7 +119,7 @@ namespace ConsoleTiny
                     var files = Directory.GetFiles(dirPath, "*.sln", SearchOption.TopDirectoryOnly);
                     if (files.Length > 0)
                     {
-                        sao.OpenEditor(files[0], fileFullPath, line);
+                        OpenVisualStudioFile(files[0], file, line);
                         return true;
                     }
                 }
@@ -292,6 +130,172 @@ namespace ConsoleTiny
             } while (true);
 
             return false;
+        }
+
+        private static void OpenVisualStudioFile(string projectPath, string file, int line)
+        {
+#if UNITY_2017_1_OR_NEWER
+            string vsPath = ScriptEditorUtility.GetExternalScriptEditor();
+#else
+            string vsPath = InternalEditorUtility.GetExternalScriptEditor();
+#endif
+            if (IsNotWindowsEditor())
+            {
+                Process.Start("open", "-a " + QuotePathIfNeeded(vsPath) + " " + QuotePathIfNeeded(file));
+                return;
+            }
+
+            if (string.IsNullOrEmpty(vsPath) || !File.Exists(vsPath))
+            {
+                return;
+            }
+            string exePath = String.Empty;
+
+#if UNITY_2018_1_OR_NEWER
+            var packageInfos = Packages.GetAll();
+            foreach (var packageInfo in packageInfos)
+            {
+                if (packageInfo.name == "com.wuhuan.consoletiny")
+                {
+                    exePath = packageInfo.resolvedPath;
+                    break;
+                }
+            }
+
+#elif UNITY_2017_1_OR_NEWER
+            // TODO
+            exePath = "../../PackagesCustom/com.wuhuan.consoletiny";
+#endif
+
+            if (!string.IsNullOrEmpty(exePath))
+            {
+                // https://github.com/akof1314/VisualStudioFileOpenTool
+                exePath = exePath + "\\Editor\\VisualStudioFileOpenTool.exe";
+                if (!File.Exists(exePath))
+                {
+                    return;
+                }
+
+                ThreadPool.QueueUserWorkItem(_ =>
+                {
+                    OpenVisualStudioFileInter(exePath, vsPath, projectPath, file, line);
+                });
+            }
+        }
+
+        private static void OpenVisualStudioFileInter(string exePath, string vsPath, string projectPath, string file, int line)
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = exePath,
+                Arguments = String.Format("{0} {1} {2} {3}",
+                    QuotePathIfNeeded(vsPath), QuotePathIfNeeded(projectPath), QuotePathIfNeeded(file), line),
+                UseShellExecute = false,
+                CreateNoWindow = true
+            });
+        }
+
+        private static bool TryOpenLuaFile(string file, int line)
+        {
+            if (IsNotWindowsEditor())
+            {
+                return false;
+            }
+            string luaPath = LuaExecutablePath();
+            if (string.IsNullOrEmpty(luaPath) || !File.Exists(luaPath))
+            {
+                return false;
+            }
+
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                OpenLuaFileInter(luaPath, file, line);
+            });
+            return true;
+        }
+
+        private static void OpenLuaFileInter(string exePath, string file, int line)
+        {
+            string arg = string.Format("{0}:{1}", QuotePathIfNeeded(file), line);
+            if (exePath.EndsWith("idea.exe", StringComparison.Ordinal) ||
+                exePath.EndsWith("idea64.exe", StringComparison.Ordinal))
+            {
+                arg = String.Format("--line {1} {0}", QuotePathIfNeeded(file), line);
+            }
+
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = exePath,
+                Arguments = arg,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            });
+        }
+
+        private static string LuaExecutablePath()
+        {
+            using (RegistryKey registryKey = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\FileExts\\.lua\\UserChoice"))
+            {
+                if (registryKey != null)
+                {
+                    string val = registryKey.GetValue("Progid") as string;
+                    if (!string.IsNullOrEmpty(val))
+                    {
+                        val = "Software\\Classes\\" + val + "\\shell\\open\\command";
+                        using (RegistryKey registryKey2 = Registry.CurrentUser.OpenSubKey(val))
+                        {
+                            string val3 = LuaExecutablePathInter(registryKey2);
+                            if (!string.IsNullOrEmpty(val3))
+                            {
+                                return val3;
+                            }
+                        }
+                    }
+                }
+            }
+
+            using (RegistryKey registryKey = Registry.ClassesRoot.OpenSubKey(".lua"))
+            {
+                if (registryKey != null)
+                {
+                    string val = registryKey.GetValue(null) as string;
+                    if (val != null)
+                    {
+                        val += "\\shell\\open\\command";
+                        using (RegistryKey registryKey2 = Registry.ClassesRoot.OpenSubKey(val))
+                        {
+                            string val3 = LuaExecutablePathInter(registryKey2);
+                            if (!string.IsNullOrEmpty(val3))
+                            {
+                                return val3;
+                            }
+                        }
+                    }
+                }
+            }
+            return String.Empty;
+        }
+
+        private static string LuaExecutablePathInter(RegistryKey registryKey2)
+        {
+            if (registryKey2 != null)
+            {
+                string val2 = registryKey2.GetValue(null) as string;
+                if (!string.IsNullOrEmpty(val2))
+                {
+                    string val3 = val2;
+                    int pos = val2.IndexOf(" \"", StringComparison.Ordinal);
+                    if (pos != -1)
+                    {
+                        val3 = val2.Substring(0, pos);
+                    }
+
+                    val3 = val3.Trim('"');
+                    return val3;
+                }
+            }
+
+            return String.Empty;
         }
     }
 }
